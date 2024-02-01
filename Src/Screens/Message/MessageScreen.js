35846a8430e7a0b2b51157fd0b62ca13b7c48611
mehmetmerthan <mehmetmerthann@gmail.com>
2024-02-01 04:46:59 +0300

    messages fixed

--- Src/Screens/Message/MessageScreen.js
+++ Src/Screens/Message/MessageScreen.js
@@ -1,37 +1,88 @@
-import { View, ScrollView, TouchableHighlight } from "react-native";
-import React from "react";
-import { ListItem, Avatar, Badge } from "@rneui/themed";
-import { useNavigation } from "@react-navigation/native";
+import { React, useState, useRef, useEffect } from "react";
+import {
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  Keyboard,
+  FlatList,
+} from "react-native";
+import { ListItem } from "@rneui/themed";
+import styles from "../../Styles/MessageDetailStyle";
+import { getUserId } from "../../Utils/getUser";
+import { API, graphqlOperation } from "aws-amplify";
+import * as subscriptions from "../../graphql/subscriptions";
+import * as queries from "../../graphql/queries";
+import { S3ImageAvatar } from "../../Components/S3Media";
 export default function MessageScreen() {
-  const navigation = useNavigation();
-  function navigateToMessageDetail() {
-    navigation.navigate("MessageDetailScreen");
-  }
+  const [messages, setMessages] = useState([]);
+  const [receiverId, setReceiverId] = useState("");
+  const scrollViewRef = useRef();
+
+  useEffect(() => {
+    async function fetchMessages() {
+      const res = await getUserId();
+      setReceiverId(res);
+      const variables = {
+        filter: {
+          or: [
+            { userMessagesReceivedId: { eq: res } },
+            { userMessagesSentId: { eq: res } },
+          ],
+        },
+      };
+      try {
+        const result = await API.graphql(
+          graphqlOperation(queries.listMessages, variables)
+        );
+        const newMessages = result?.data?.listMessages?.items;
+        setMessages(newMessages);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchMessages();
+    const subscription = API.graphql(
+      graphqlOperation(subscriptions.onCreateMessage)
+    ).subscribe({
+      next: ({ value }) => {
+        const newMessage = value.data.onCreateMessage;
+        setMessages((prevMessages) => [...prevMessages, newMessage]);
+        scrollViewRef.current.scrollToEnd({ animated: true });
+      },
+      error: (error) => console.log(error),
+    });
+    return () => subscription.unsubscribe();
+  }, []);
+  const handleKeyboardDismiss = () => {
+    Keyboard.dismiss();
+    console.log("Keyboard dismissed");
+  };
+  const renderMessage = ({ item }) => {
+    return (
+      <>
+        <ListItem bottomDivider>
+          <S3ImageAvatar imageKey={item.key_pp} size="small" />
+          <ListItem.Content>
+            <ListItem.Title>{item.name}</ListItem.Title>
+            <ListItem.Subtitle>{item.message}</ListItem.Subtitle>
+          </ListItem.Content>
+        </ListItem>
+      </>
+    );
+  };
   return (
-    <ScrollView>
-      <ListItem
-        bottomDivider
-        Component={TouchableHighlight}
-        onPress={navigateToMessageDetail}
-      >
-        <View>
-          <Avatar
-            rounded
-            source={{ uri: "https://randomuser.me/api/portraits/men/36.jpg" }}
-            size="medium"
-          />
-          <Badge
-            status="primary"
-            value={1}
-            containerStyle={{ position: "absolute", top: 1, left: 40 }}
-          />
-        </View>
-        <ListItem.Content>
-          <ListItem.Title>John Doe</ListItem.Title>
-          <ListItem.Subtitle>President</ListItem.Subtitle>
-        </ListItem.Content>
-        <ListItem.Chevron color="black" />
-      </ListItem>
-    </ScrollView>
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={handleKeyboardDismiss}>
+        <FlatList
+          data={messages}
+          renderItem={renderMessage}
+          keyExtractor={(item) => item.id}
+          ref={scrollViewRef}
+          onContentSizeChange={() =>
+            scrollViewRef.current.scrollToEnd({ animated: true })
+          }
+        />
+      </TouchableWithoutFeedback>
+    </View>
   );
 }
